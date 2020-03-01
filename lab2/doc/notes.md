fefb003b28627aeed1473d82c0f9e002d115a0e6
Wojciech Dobkowicz <37017975+wdobkowicz@users.noreply.github.com>
2020-03-01 15:12:44 +0100

    Add files via upload

---
+++ lab2/doc/notes.md
@@ -0,0 +1,9 @@
+# Notatki dotyczące problemu
+
+## Dane wejściowe:
+
+3 zbiory liczb całkowitych o tej samej długości k oraz liczba ograniczająca b.
+
+## Dane wyjsciowe
+
+Rozwiązanie - podzbiór M spełniajacy określone warunki.
---
+++ lab2/n3dm-problem.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+///zadanie do rozwiązania
+
+int k,b;
+class zbior_t {
+public:
+  int x, y, z;
+};
+using problem_t = vector<zbior_t>;
+
+class n3dm_solution_ts{
+
+};
+
+
+int main() {
+    using namespace std;
+    problem_t problem = {
+        {3,4,4},
+        {1,4,6},
+        {1,2,5}
+    };
+
+    return 0;
+}
